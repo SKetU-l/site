79168d543a33adf0ff3fd8d0a7008a54bcf4af75
Shemmy <Shemmynyirenda3@gmail.com>
2024-06-18 14:54:01 +0200

    added a navbar of new desing

--- components/App/Navbar.vue
+++ components/App/Navbar.vue
@@ -1,15 +1,24 @@
 <template>
-  <div class="navbar">
-    <a href="/" class="logo">RisingOS</a>
+  <div
+    class="min-h-14 w-full px-4 py-2 flex justify-between items-center bg-colorBackgroundLight dark:bg-colorBackgroundDark"
+  >
+    <a href="/" class="w-12 h-12 p-4 items-center"
+      ><img src="/public/logo-dark.svg" alt="RisingOS-Logo"
+    /></a>
     <nav>
-      <ul>
-        <li v-for="page in pages">
-          <a :href="page.path">{{ page.name }}</a>
+      <ul class="flex flex-row space-x-1">
+        <li
+          v-for="page in pages"
+          class="px-2 py-1 rounded-full hover:bg-colorPrimaryLight hover:bg-opacity-30 transition-all duration-200"
+        >
+          <NuxtLink class="hover:text-colorPrimaryLight hover:font-bold" :to="page.path">
+            {{ page.name }}
+          </NuxtLink>
         </li>
       </ul>
     </nav>
     <button
-      class="bg-colorTextDark dark:bg-colorTextLight text-colorTextDark dark:text-colorTextLight"
+      class="min-h-12 px-4 py-2 rounded-full bg-colorBackgroundDark dark:bg-colorBackgroundLight text-colorTextDark dark:text-colorTextLight"
     >
       Download
     </button>
