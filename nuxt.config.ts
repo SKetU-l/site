bc55d82885b0fa3bd2ffb3ed5df6b6d4a4a53467
Shemmy <Shemmynyirenda3@gmail.com>
2024-07-13 18:04:50 +0200

    added base url and tab logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    baseURL: "/site/",
+    head: {
+      link: [
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/logo.svg",
+        },
+      ],
+    },
+  },
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "nuxt-icon"],
   components: [
