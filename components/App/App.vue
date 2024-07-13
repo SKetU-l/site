2c0a7d36015fce1463263cf6e965a218ebc3dc36
Shemmy <Shemmynyirenda3@gmail.com>
2024-07-13 15:21:14 +0200

    use new dynamic units

--- components/App/App.vue
+++ components/App/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="min-w-full min-h-[100dvh] select-none">
+  <div
+    class="flex flex-col w-full min-h-[100dvh] select-none bg-colorBackgroundLight dark:bg-colorBackgroundDark text-colorTextLight dark:text-colorTextDark"
+  >
     <slot />
   </div>
 </template>
